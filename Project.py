ee3fdc767a7609ffa45c383f31ff574f94804d48
MatiasGarciaGutierrez <matias.garcia.g@ing.uchile.cl>
2018-11-11 23:42:25 -0300

    Se agrega documentación

--- Project.py
+++ Project.py
@@ -14,8 +14,11 @@ vgg_model_fc7 = Model(vgg_model.input, out)
 
 
 def get_features(image_path):
+    """
 
-    # Change the image path with yours.
+    :param image_path(str): Path of a image to extract features with VGG-face.
+    :return: A 4096 feature vector.
+    """
     img = image.load_img(image_path, target_size=(224, 224))
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
@@ -25,6 +28,11 @@ def get_features(image_path):
 
 
 def create_all_features(path):
+    """
+
+    :param path: path of the divided dataset.
+    :return: two list of all young and old photos descriptors.
+    """
     young_descriptors = []
     old_descriptors = []
     for folder in os.listdir(path):
@@ -35,6 +43,13 @@ def create_all_features(path):
 
 
 def create_features(path, age_group, list):
+    """
+
+    :param path: path of a folder with images.
+    :param age_group: young/old.
+    :param list: list of descriptors.
+    :return: the list receive with new features append.
+    """
     for image in os.listdir(path+"/"+age_group):
         label = float(image[0:3])
         age = float(image[4:6])
@@ -46,6 +61,12 @@ def create_features(path, age_group, list):
 
 
 def save_features(old_features, young_features):
+    """
+
+    :param old_features: features of old ages
+    :param young_features: features of young ages
+    :return: nothing.
+    """
     savemat('datosImagenes.mat', {'old': old_features, 'young': young_features})
     return
 
